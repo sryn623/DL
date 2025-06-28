0bbe9cf5aaa322f11d3e67ea3849a755d61c817d
Shreyan Wasti <115867311+sryn623@users.noreply.github.com>
2025-06-28 20:06:55 +0545

    IndexingSlicing

---
+++ Course1/indexingvector.py
@@ -0,0 +1,22 @@
+import numpy as np
+import time 
+
+#vector slicing operations
+a = np.arange(10)
+print(f"a         = {a}")
+
+#access 5 consecutive elements (start:stop:step)
+c = a[2:7:1];     print("a[2:7:1] = ", c)
+
+# access 3 elements separated by two 
+c = a[2:7:2];     print("a[2:7:2] = ", c)
+
+# access all elements index 3 and above
+c = a[3:];        print("a[3:]    = ", c)
+
+# access all elements below index 3
+c = a[:3];        print("a[:3]    = ", c)
+
+# access all elements
+c = a[:];         print("a[:]     = ", c)
+
