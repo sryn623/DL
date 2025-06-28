2eeb5d941a4daa568f2fcdb519c04c80d6648a6b
Shreyan Wasti <115867311+sryn623@users.noreply.github.com>
2025-06-28 19:45:18 +0545

    numpybasic

--- Course1/num.py
+++ Course1/num.py
@@ -1,2 +1,5 @@
-import numpy as np
-print("NumPy version:", np.__version__)
+import numpy as np    
+import time
+# NumPy routines which allocate memory and fill with user specified values
+a = np.array([5,4,3,2]);  print(f"np.array([5,4,3,2]):  a = {a},     a shape = {a.shape}, a data type = {a.dtype}")
+a = np.array([5.,4,3,2]); print(f"np.array([5.,4,3,2]): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
