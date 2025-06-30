6f7f52007ecd7962bdd80f15c7eed6b368d4d8ea
Shreyan Wasti <115867311+sryn623@users.noreply.github.com>
2025-06-30 19:24:44 +0545

    Predict single loop

---
+++ Course1/multiplevar.py
@@ -0,0 +1,36 @@
+import copy, math
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('./deeplearning.mplstyle')
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
+y_train = np.array([460, 232, 178])
+
+def predict_single_loop(x, w, b): 
+    """
+    single predict using linear regression
+    
+    Args:
+      x (ndarray): Shape (n,) example with multiple features
+      w (ndarray): Shape (n,) model parameters    
+      b (scalar):  model parameter     
+      
+    Returns:
+      p (scalar):  prediction
+    """
+    n = x.shape[0]
+    p = 0
+    for i in range(n):
+        p_i = x[i] * w[i]  
+        p = p + p_i         
+    p = p + b                
+    return p
+
+# get a row from our training data
+x_vec = X_train[0,:]
+print(f"x_vec shape {x_vec.shape}, x_vec value: {x_vec}")
+
+# make a prediction
+f_wb = predict_single_loop(x_vec, w_init, b_init)
+print(f"f_wb shape {f_wb.shape}, prediction: {f_wb}")
