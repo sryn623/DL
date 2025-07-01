66a4b540ae2c5d2d940cccf8dc0c54490687783d
Shreyan Wasti <115867311+sryn623@users.noreply.github.com>
2025-07-01 19:41:56 +0545

    No package

--- Course1/featurescaling.py
+++ Course1/featurescaling.py
@@ -10,9 +10,27 @@ plt.style.use('./deeplearning.mplstyle')
 X_train, y_train = load_house_data()
 X_features = ['size(sqft)','bedrooms','floors','age']
 
-fig,ax=plt.subplots(1, 4, figsize=(12, 3), sharey=True)
-for i in range(len(ax)):
-    ax[i].scatter(X_train[:,i],y_train)
-    ax[i].set_xlabel(X_features[i])
-ax[0].set_ylabel("Price (1000's)")
-plt.show()
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+    
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+      
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    # find the mean of each column/feature
+    mu     = np.mean(X, axis=0)                 # mu will have shape (n,)
+    # find the standard deviation of each column/feature
+    sigma  = np.std(X, axis=0)                  # sigma will have shape (n,)
+    # element-wise, subtract mu for that column from each example, divide by std for that column
+    X_norm = (X - mu) / sigma      
+
+    return (X_norm, mu, sigma)
+ 
+#check our work
+#from sklearn.preprocessing import scale
+#scale(X_orig, axis=0, with_mean=True, with_std=True, copy=True)
